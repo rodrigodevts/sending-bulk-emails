d73c3a3dfa8b5672e28b2f98087129010828a612
rodrigodevts <rodrigo.sousa.devjs@gmail.com>
2024-12-12 13:56:24 -0300

    feat: send message e-mail

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,20 @@
+import { SendMessageService } from '@services/SendMessageService';
 import { Router } from 'express';
 
 const routes = Router();
+
+routes.get('/status', async (req, res) => {
+	res.status(200).json({
+		status: 'OK'
+	});
+});
+
+routes.post('/messages', async (req, res) => {
+	const sendMessage = new SendMessageService();
+
+	await sendMessage.run();
+
+	res.json({ ok: true });
+})
 
 export default routes;
